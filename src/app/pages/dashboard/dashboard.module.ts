ee7f5886749af504ea6318b61cb1a8c550590bd0
eyad <eyiadhisham@yahoo.com>
2020-06-11 02:22:51 +0200

    line chart component skeleton

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -6,17 +6,20 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { SimpleCardComponent } from './simple-card/simple-card.component';
 import { UsersChartComponent } from './users-chart/users-chart.component';
-
+import { LineChartComponent } from './../dashboard/line-chart/line-chart.component';
+import { ChartjsModule } from '@ctrl/ngx-chartjs';
 @NgModule({
   imports: [
     NbCardModule,
     ThemeModule,
     NgxChartsModule,
+    ChartjsModule
   ],
   declarations: [
     DashboardComponent,
     SimpleCardComponent,
     UsersChartComponent,
+    LineChartComponent
   ],
 })
 export class DashboardModule { }
