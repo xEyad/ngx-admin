d12b3165e96c7f6f35f95ce36d412ab41ce065ee
eyad <eyiadhisham@yahoo.com>
2020-06-11 22:46:43 +0200

    social cards

---
+++ src/app/pages/dashboard/social-cards/social-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-social-cards',
+  templateUrl: './social-cards.component.html',
+  styleUrls: ['./social-cards.component.scss']
+})
+export class SocialCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
