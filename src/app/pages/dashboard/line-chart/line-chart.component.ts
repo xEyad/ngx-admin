51f0cbe06b378018a213d29665d22f4528cef47f
eyad <eyiadhisham@yahoo.com>
2020-06-12 17:20:16 +0200

    chart fills parent size

--- src/app/pages/dashboard/line-chart/line-chart.component.ts
+++ src/app/pages/dashboard/line-chart/line-chart.component.ts
@@ -6,32 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.scss']
 })
 export class LineChartComponent implements OnInit {
+  options={
+    maintainAspectRatio: false,
+    responsive: true,
+    legend:{display: true, position: 'top'}
+
+  }
    data = {
     labels: ['يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو'],
     datasets: [
       {
         label: 'الزائرون الجدد',
         data: [65, 59, 80, 81, 56, 55, 40],
-        maintainAspectRatio: false,
         fill: true,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 205, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(201, 203, 207, 0.2)',
-        ],
-        borderColor: [
-          'rgb(255, 99, 132)',
-          'rgb(255, 159, 64)',
-          'rgb(255, 205, 86)',
-          'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)',
-          'rgb(201, 203, 207)',
-        ],
+        backgroundColor:'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 1,
+      },
+      {
+        label: 'الزائرون الِقدام',
+        data: [15, 232, 3, 666, 22, 42, 61],
+        fill: true,
+        backgroundColor:'rgba(15, 2, 61, 0.2)',
+        borderColor: 'rgb(5, 2, 61)',
         borderWidth: 1,
       },
     ],
