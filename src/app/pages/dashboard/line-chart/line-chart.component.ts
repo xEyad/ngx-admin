359d2d269d4c64a55a92a800f9a9c0859584e79e
eyad <eyiadhisham@yahoo.com>
2020-06-12 20:00:11 +0200

    better colors and ux

--- src/app/pages/dashboard/line-chart/line-chart.component.ts
+++ src/app/pages/dashboard/line-chart/line-chart.component.ts
@@ -9,26 +9,28 @@ export class LineChartComponent implements OnInit {
   options={
     maintainAspectRatio: false,
     responsive: true,
-    legend:{display: true, position: 'top'}
+    legend:{display: true, position: 'top'},
+    tooltips:{intersect:false,mode:'index'}
 
   }
    data = {
-    labels: ['يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو'],
+    labels: ['January', 'February', 'Mars', 'April', 'May', 'June', 'July'],
+    // labels: ['يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو'],
     datasets: [
       {
-        label: 'الزائرون الجدد',
+        label: 'New visitors',
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: true,
-        backgroundColor:'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor:'rgba(69, 67, 114, 0.5)',
+        borderColor: 'rgb(69, 67, 114)',
         borderWidth: 1,
       },
       {
-        label: 'الزائرون الِقدام',
+        label: 'Old visitors',
         data: [15, 232, 3, 666, 22, 42, 61],
         fill: true,
-        backgroundColor:'rgba(15, 2, 61, 0.2)',
-        borderColor: 'rgb(5, 2, 61)',
+        backgroundColor:'rgba(102, 78, 76, 0.2)',
+        borderColor: 'rgb(102, 78, 76)',
         borderWidth: 1,
       },
     ],
