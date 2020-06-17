2b3dde96145df0b7cbaab82952f59c2c59c648cd
eyad <eyiadhisham@yahoo.com>
2020-06-17 03:08:37 +0200

    more data for socials

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+  facebokInfo =  Array(6).fill(
+    {
+      name:'Engagment',
+      value:"450,560",
+      icon:"fas fa-users"
+    });
 }
