8e6ce6e370369112a49f74ed5427042568b5ba53
eyad <eyiadhisham@yahoo.com>
2020-06-17 21:06:13 +0200

    social info

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,10 +5,95 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
-  facebokInfo =  Array(6).fill(
+  facebokInfo:{name,value,icon}[] =  [
     {
       name:'Engagment',
       value:"450,560",
       icon:"fas fa-users"
-    });
+    },
+    {
+      name:'Likes',
+      value:"560",
+      icon:"fas fa-thumbs-up"
+    },
+    {
+      name:'Reach',
+      value:"560",
+      icon:"fas fa-project-diagram"
+    },
+    {
+      name:'Messages',
+      value:"560",
+      icon:"fas fa-comments"
+    },
+    {
+      name:'Clicks',
+      value:"560",
+      icon:"fas fa-mouse-pointer"
+    },
+    {
+      name:'Posts',
+      value:"560",
+      icon:"fas fa-pager"
+    },
+
+  ];
+
+  twitterInfo:{name,value,icon}[] =  [
+    {
+      name:'Retweets',
+      value:"450,560",
+      icon:"fas fa-retweet"
+    },
+    {
+      name:'Engagment',
+      value:"450,560",
+      icon:"fas fa-users"
+    },
+    {
+      name:'Likes',
+      value:"560",
+      icon:"fas fa-thumbs-up"
+    },
+    {
+      name:'Impressions',
+      value:"560",
+      icon:"fas fa-project-diagram"
+    },
+    {
+      name:'Clicks',
+      value:"560",
+      icon:"fas fa-mouse-pointer"
+    },
+    {
+      name:'Posts',
+      value:"560",
+      icon:"fas fa-pager"
+    },
+
+  ];
+
+  youtubeInfo:{name,value,icon}[] =  [
+    {
+      name:'Subscribers',
+      value:"450,560",
+      icon:"fas fa-users"
+    },
+    {
+      name:'Likes',
+      value:"560",
+      icon:"fas fa-thumbs-up"
+    },
+    {
+      name:'Views',
+      value:"560",
+      icon:"fas fa-eye"
+    },
+    {
+      name:'Videos',
+      value:"560",
+      icon:"fas fa-video"
+    },
+
+  ];
 }
