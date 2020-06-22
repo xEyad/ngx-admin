85a45a0346def464b9dbc74a387f6054d8154026
eyad <eyiadhisham@yahoo.com>
2020-06-22 15:56:09 +0200

    rtl app

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -62,7 +62,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
     private userService:UsersService,
     private nbMenuService: NbMenuService,
     private authService:AuthenticationService,
-    private directionService:NbLayoutDirectionService,
     private router:Router
   ) {
     this.materialTheme$ = this.themeService.onThemeChange()
@@ -73,7 +72,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   async ngOnInit() {
-    this.directionService.setDirection(NbLayoutDirection.RTL);
     this.currentTheme = this.themeService.currentTheme;
 
     this.user = await this.userService.getCurrentUser();
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { AuthenticationService } from './../services/authentication.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { NbToastrService } from '@nebular/theme';
+import { NbToastrService, NbLayoutDirectionService, NbLayoutDirection } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-login',
@@ -15,10 +15,12 @@ export class LoginComponent implements OnInit {
   constructor(
     private router:Router,
     private auth:AuthenticationService,
+    private directionService:NbLayoutDirectionService,
     private toast:NbToastrService
     ) { }
 
   ngOnInit(): void {
+    this.directionService.setDirection(NbLayoutDirection.RTL);
   }
   async login()
   {
