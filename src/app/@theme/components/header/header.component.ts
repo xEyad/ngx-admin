afef4bf0214e316676a30d2a1957a284dc7d94b8
eyad <eyiadhisham@yahoo.com>
2020-06-22 16:34:37 +0200

    roles power

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -62,6 +62,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     private userService:UsersService,
     private nbMenuService: NbMenuService,
     private authService:AuthenticationService,
+    private directionService:NbLayoutDirectionService,
     private router:Router
   ) {
     this.materialTheme$ = this.themeService.onThemeChange()
@@ -72,6 +73,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   async ngOnInit() {
+    this.directionService.setDirection(NbLayoutDirection.RTL);
     this.currentTheme = this.themeService.currentTheme;
 
     this.user = await this.userService.getCurrentUser();
